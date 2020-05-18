3d492911b2e67306ecf243bf5e37165c534f70fa
BoziDaniel <daniel.bozi88@gmail.com>
2020-05-18 17:57:42 +0200

    initiated database

---
+++ src/main/java/com/codecool/pizzabackend/entity/Pizza.java
@@ -0,0 +1,28 @@
+package com.codecool.pizzabackend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Pizza {
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+}
