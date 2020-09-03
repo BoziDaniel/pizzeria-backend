809187f941276e9f7535bc2e00d07c16a60ddf30
MiklosHorvath <horvathmikloswork@gmail.com>
2020-09-03 13:14:37 +0200

    refactor DeliveryGuyController, added DeliverGuyService

--- src/main/java/com/codecool/pizzabackend/controller/DeliveryGuyController.java
+++ src/main/java/com/codecool/pizzabackend/controller/DeliveryGuyController.java
@@ -1,36 +1,26 @@
 package com.codecool.pizzabackend.controller;
 
 import com.codecool.pizzabackend.controller.dto.UserDTO;
-import com.codecool.pizzabackend.repository.UserRepository;
+import com.codecool.pizzabackend.service.DeliveryGuyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/deliveryGuy")
 public class DeliveryGuyController {
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderrController.class);
     @Autowired
-    private UserRepository userRepository;
-
-    public DeliveryGuyController(UserRepository userRepository) {
-        this.userRepository=userRepository;
-    }
+    private DeliveryGuyService deliveryGuyService;
 
     @GetMapping("/all")
     public List<UserDTO> getAllDeliveryGuys() {
         LOGGER.info("Get request: /deliveryGuy/all arrived");
-        //Todo: use sevice layer at least, but best to use db query instead filter
-        List<UserDTO> deliveryGuys = userRepository.findAll().stream()
-                .filter(user -> user.getRoles().contains("ROLE_DELIVERYGUY"))
-                .map(deliveryGuy -> deliveryGuy.createDTO())
-                .collect(Collectors.toList());
+        List<UserDTO> deliveryGuys = deliveryGuyService.getAllDeliveryGuys();
         LOGGER.info(" Get request: /deliveryGuy/all processed. Return value will be: " + deliveryGuys.toString());
         return deliveryGuys;
     }
---
+++ src/main/java/com/codecool/pizzabackend/service/DeliveryGuyService.java
@@ -0,0 +1,24 @@
+package com.codecool.pizzabackend.service;
+
+import com.codecool.pizzabackend.controller.dto.UserDTO;
+import com.codecool.pizzabackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class DeliveryGuyService {
+    @Autowired
+    private UserRepository userRepository;
+
+    //Todo: best to use db query instead filter
+    public List<UserDTO> getAllDeliveryGuys(){
+        List<UserDTO> deliveryGuys = userRepository.findAll().stream()
+                .filter(user -> user.getRoles().contains("ROLE_DELIVERYGUY"))
+                .map(deliveryGuy -> deliveryGuy.createDTO())
+                .collect(Collectors.toList());
+        return deliveryGuys;
+    }
+}
