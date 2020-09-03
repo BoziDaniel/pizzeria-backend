062b1dd8f4def32314d5b9ec3659439dcff331dd
MiklosHorvath <horvathmikloswork@gmail.com>
2020-09-03 13:05:17 +0200

    refactor CookController class, add CookService

---
+++ src/main/java/com/codecool/pizzabackend/service/CookService.java
@@ -0,0 +1,24 @@
+package com.codecool.pizzabackend.service;
+
+import com.codecool.pizzabackend.controller.dto.UserDTO;
+import com.codecool.pizzabackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CookService {
+    @Autowired
+    private UserRepository userRepository;
+
+    //Todo: best to use db query instead filter
+    public List<UserDTO> getAllCooks(){
+        List<UserDTO> cooks = userRepository.findAll().stream()
+                .filter(user -> user.getRoles().contains("ROLE_COOK"))
+                .map(cook -> cook.createDTO())
+                .collect(Collectors.toList());
+        return cooks;
+    }
+}
