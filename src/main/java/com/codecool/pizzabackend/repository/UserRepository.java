5863f04692b75228b394d60ef00be5d969f25cee
MiklosHorvath <horvathmikloswork@gmail.com>
2020-09-10 12:31:14 +0200

    implements endpoint for checking if user exitst with username

--- src/main/java/com/codecool/pizzabackend/repository/UserRepository.java
+++ src/main/java/com/codecool/pizzabackend/repository/UserRepository.java
@@ -8,4 +8,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User getUserById(Long Id);
 
     Optional<User> getAppUserByUsername(String username);
+
+
+    boolean existsByUsername(String username);
 }
